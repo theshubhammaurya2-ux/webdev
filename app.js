2998adcbefbe26a71d7ae6907ec9ce36bee740cd
theshubhammaurya2-ux <theshubhammaurya2@gmail.com>
2026-09-20 22:51:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+console.log("shubham maurya")
+// console.log("linking file with the html")
+// name="shubham maurya ";
+// console.log(name)
+
+// age=24;
+// console.log(age);
+// price=null;
+// price=undefined;
+// console.log(price);
+// isfollow=true;
+// console.log(isfollow)
+
+
+var age=89;
+let price=90;
+console.log(price);
+const pi=3.13;
+console.log(pi);
+typeof pi
+
+let x=BigInt("123");
+let y=Symbol("hello");
+
+
+
+
+
+// const student={
+//     name:"shubham",
+//     age:20,
+//     cgpa:90,
+//     ispass:true,
+// };
+// student["age"]=student["age"]+1;
+// console.log(student["age"]);
+// console.log(student.age);
+//  student["age"]=56;
+
+
+
+
+
